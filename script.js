3c27e335dc9426fee20a87eb6dfe33a7fa11c206
dav1dborja <davidborjamartinez@gmail.com>
2021-03-29 13:40:06 +0200

    adding the timer

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const word = document.getElementById("word");
 const text = document.getElementById("text");
 const scoreEl = document.getElementById("score");
 const timeEl = document.getElementById("time");
-const endgameEl = document.getElementById("end-game");
+const endgameEl = document.getElementById("end-game-container");
 const settingsBtn = document.getElementById("settings-btn");
 const settings = document.getElementById("settings");
 const settingsForm = document.getElementById("settings-form");
@@ -41,6 +41,12 @@ let score = 0;
 // Init time
 let time = 10;
 
+// Focus on text on start
+text.focus();
+
+// Start Counting Down
+const timeInterval = setInterval(updateTime, 1000);
+
 // Generate random word from array
 function getRandomWord() {
   return words[Math.floor(Math.random() * words.length)];
@@ -58,9 +64,32 @@ function updateScore() {
   scoreEl.innerHTML = score;
 }
 
-addWordToDOM()
+// Update time
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time + 's';
 
-text.addEventListener("input", e => {
+  if(time === 0) {
+    clearInterval(timeInterval);
+    // end game
+    gameOver();
+  }
+}
+
+// Game over, show end screen
+function gameOver() {
+  endgameEl.innerHTML = `
+    <h1>Time ran out</h1>
+    <p>Your final score is ${score}</p>
+    <button onclick="location.reload()">Reload</button>
+  `;
+
+  endgameEl.style.display = 'flex';
+}
+
+addWordToDOM();
+
+text.addEventListener("input", (e) => {
   const insertedText = e.target.value;
 
   if (insertedText === randomWord) {
@@ -68,7 +97,11 @@ text.addEventListener("input", e => {
     updateScore();
 
     // Clear
-    e.target.value = ""
+    e.target.value = "";
+
+    time += 5;
+
+    updateTime();
     /* text.value = "" this would have been my try */
   }
 });
